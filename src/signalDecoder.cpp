831f3c3f1aaf229028f7a830518104b756e2456e
wastlmuc <101905304+wastlmuc@users.noreply.github.com>
2024-03-30 20:43:46 +0100

    Update signalDecoder.cpp

--- src/signalDecoder.cpp
+++ src/signalDecoder.cpp
@@ -327,7 +327,7 @@ void rtlSetup() {
     // end of fragment
 
 #else
-    memcpy(&cfg->devices[0], &lacrosse_tx141x, sizeof(r_device));
+    memcpy(&cfg->devices[107], &prologue, sizeof(r_device));
 #endif
 
 #ifdef RTL_FLEX
